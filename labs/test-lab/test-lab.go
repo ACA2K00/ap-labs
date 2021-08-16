8faf927db4b136ef6735b2445e6e79f1c707a91c
Alejandro Castro <alexjr00@live.com.mx>
2021-08-16 21:20:29 +0000

    Solved test-lab

--- labs/test-lab/test-lab.go
+++ labs/test-lab/test-lab.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Welcome to the jungle")
+	name := os.Args[1:]
+	if len(os.Args) == 1 {
+		fmt.Printf("No name submitted, submit your name please... \n")
+	}else{
+		fmt.Printf("Welcome %v to the jungle \n", name)
+	}
 }
