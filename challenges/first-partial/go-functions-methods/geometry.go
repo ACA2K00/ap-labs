48c2ba70a743f0b9d6b853b4d759fead75a110b3
Alejandro Castro <alexjr00@live.com.mx>
2021-09-07 02:30:00 +0000

    Segment Intersection Validated

--- challenges/first-partial/go-functions-methods/geometry.go
+++ challenges/first-partial/go-functions-methods/geometry.go
@@ -11,6 +11,7 @@ import (
 )
 
 type Point struct{ x, y float64 }
+type Segment struct{p []Point}
 
 // traditional function
 func Distance(p, q Point) float64 {
@@ -32,13 +33,83 @@ func (p *Point) Y(n float64) float64{
 	return p.y
 }
 
-func main() {
+//Intersection validation start
+func onSegment(p, q, r Point) bool{
+	if (q.x <= math.Max(p.x, r.x) && q.x >= math.Min(p.x, r.x) &&
+	q.y <= math.Max(p.y, r.y) && q.y >= math.Min(p.y, r.y)){
+		return true
+	}else{
+		return false
+	}
+}
+
+func orientation(p, q, r Point) int{
+	val := (q.y - p.y) * (r.x - q.x) -(q.x - p.x) * (r.y - q.y)
+	if val == 0 {
+		return 0
+	}
+	
+	if(val > 0){
+		return 1
+	}else{
+		return 2
+	}
+}
+
+func doIntersect(p1, q1, p2, q2 Point) bool{
+	//4 orientations
+	o1  := orientation(p1, q1, p2)
+	o2  := orientation(p1, q1, q2)
+	o3  := orientation(p2, q2, p1)
+	o4  := orientation(p2, q2, q1)
+
+	//General case
+	if o1 != o2 && o3 != o4 {return true}
+
+	//Special cases
+	if (o1 == 0 && onSegment(p1, p2, q1)) {return true;}
+	if (o2 == 0 && onSegment(p1, q2, q1)) {return true;}
+	if (o3 == 0 && onSegment(p2, p1, q2)) {return true;}
+	if (o4 == 0 && onSegment(p2, q1, q2)) {return true;}
+
+	return false
+}
+//Intersection validation end
+
+func reroll(ps []Point, n int){
 	var point Point
+	rand.Seed(time.Now().UnixNano())
+    	min := -100
+    	max := 100
+
+		for i := 0; i < n; i++{
+			newX := rand.Intn(max-min) + min
+			newY := rand.Intn(max-min) + min
+			point.X(float64(newX))
+			point.Y(float64(newY))
+			ps[i] = point
+		}
+}
+
+func listSegments(ss []Segment, ps []Point, n int){
+	line := make([]Point, 2)
+	for i := 0; i < n-1; i++{
+		line[0] = ps[i]
+		line[1] = ps[i+1]
+		ss[i] = Segment{line}
+	}
+	line[0] = ps[n-1]
+	line[1] = ps[0]
+	ss[n-1] = Segment{line}
+}
+
+func main() {
 	sum := 0.0
 
 	if len(os.Args) == 1{
 		fmt.Printf("No value submitted, submit a number please... \n")
 	}else{
+
 		sides, err := strconv.Atoi(os.Args[1])
 		if err != nil {
 			fmt.Println(err)
@@ -46,25 +117,63 @@ func main() {
 		}
 	
 		points := make([]Point, sides)
+		segments := make([]Segment, sides)
 		dists := make([]float64, sides)
-
-		rand.Seed(time.Now().UnixNano())
-    	min := -100
-    	max := 100
-
-		for i := 0; i < sides; i++{
-			newX := rand.Intn(max-min) + min
-			newY := rand.Intn(max-min) + min
-			point.X(float64(newX))
-			point.Y(float64(newY))
-			points[i] = point
-		}
-
+		
 		fmt.Printf("Sides: %v\n", sides)
+		
+		//Point Slice - start
+		reroll(points, sides) //fills point slice
+
 		for i := 0; i < sides; i++{
 			fmt.Printf("Point %v: %v\n", i, points[i])
 		}
+		//Point Slice - end
 
+		listSegments(segments, points, sides)
+
+		//Intersection checking & fixing - start
+		
+		k := 0
+		for i := 0; i < sides-1; i++{
+			p1 := segments[i].p[0]
+			q1 := segments[i].p[1]
+			p2 := segments[i+1].p[0]
+			q2 := segments[i+1].p[1]
+			if(doIntersect(p1, q1, p2, q2)){
+				fmt.Printf("Segment Intersection\n")
+				reroll(points, sides)
+				listSegments(segments, points, sides)
+
+				for i := 0; i < sides; i++{
+					fmt.Printf("Point %v: %v\n", i, points[i])
+				}
+
+				i = -1
+				fmt.Printf("Attempt : %v\n", k)
+				k++
+				if k == 100 {
+					fmt.Printf("Too many unsuccessful attempts, killing process...\n")
+					break
+					}				
+			}else{
+				for i := 0; i < sides-1; i++{
+					dist := points[i].Distance(points[i+1])
+					dists[i] = dist
+				}
+				ldist := points[sides-1].Distance(points[0])
+				dists[sides-1] = ldist
+		
+				for i := 0; i < sides; i++{
+					sum += dists[i]
+				}
+				fmt.Printf("Perimeter: %v \n", sum)
+			}
+		}
+		//Intersection checking & fixing - end
+		
+
+		/*
 		for i := 0; i < sides-1; i++{
 			dist := points[i].Distance(points[i+1])
 			dists[i] = dist
@@ -76,5 +185,8 @@ func main() {
 			sum += dists[i]
 		}
 		fmt.Printf("Perimeter: %v \n", sum)
+		*/
+
 	}
+	
 }
