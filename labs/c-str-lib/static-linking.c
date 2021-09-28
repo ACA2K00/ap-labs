995abf37941f5e94f97b74e60f4d67002d2d9a2e
Alejandro Castro <alexjr00@live.com.mx>
2021-09-28 23:58:59 +0000

    Solved static linking

--- labs/c-str-lib/static-linking.c
+++ labs/c-str-lib/static-linking.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+int add(int, int);
+int mystrlen(char *s);
+char *mystradd(char *o, char *a);
+int mystrfind(char *o, char *f);
+
 int main() {
+    printf("2+3=%d\n", add(2,3));
+    printf("Blue = %d\n", mystrlen("Blue"));
+    printf("Origin + Add = %s\n", mystradd("Origin", "Add"));
+    printf("Azul Rojo Verde, Verde in: %d\n", mystrfind("Azul Rojo Verde", "Azul"));
     return 0;
 }
--- labs/c-str-lib/strlib.c
+++ labs/c-str-lib/strlib.c
@@ -1,13 +1,56 @@
 
 
 int mystrlen(char *str){
-    return 0;
+    int cont = 0;
+    for (int i = 0; str[i] != '\0'; ++i){
+        ++cont;
+    }
+    return cont;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    int cont = 0;
+    static char res[100];
+    for (int i = 0; origin[i] != '\0'; ++i){
+        res[i]=origin[i];
+        ++cont;
+    }
+    for (int i = 0; addition[i] != '\0'; ++i){
+        res[cont]=addition[i];
+        ++cont;
+    }
+    return res;
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+    int i, j, firstOcc;
+   i = 0, j = 0;
+ 
+   while (origin[i] != '\0') {
+ 
+      while (origin[i] != substr[0] && origin[i] != '\0')
+         i++;
+ 
+      if (origin[i] == '\0')
+         return (-1);
+ 
+      firstOcc = i;
+ 
+      while (origin[i] == substr[j] && origin[i] != '\0' && substr[j] != '\0') {
+         i++;
+         j++;
+      }
+ 
+      if (substr[j] == '\0')
+         return (firstOcc);
+      if (origin[i] == '\0')
+         return (-1);
+ 
+      i = firstOcc + 1;
+      j = 0;
+   }
+}
+
+int add(int a, int b) {
+    return a + b;
 }
